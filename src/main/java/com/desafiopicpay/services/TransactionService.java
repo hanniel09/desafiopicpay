7c52d201a7886e9568752432a318bb2f873cc9b9
hanniel09 <hannielvieira1227@gmail.com>
2023-10-30 23:27:41 -0300

    feat: create Transaction service

---
+++ src/main/java/com/desafiopicpay/services/TransactionService.java
@@ -0,0 +1,45 @@
+package com.desafiopicpay.services;
+
+import com.desafiopicpay.domain.user.User;
+import com.desafiopicpay.dtos.TransactionDto;
+import com.desafiopicpay.repositories.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+@Service
+public class TransactionService {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private TransactionRepository repository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private void createTransaction(TransactionDto transaction) throws Exception {
+        User sender = this.userService.findUserById(transaction.senderId());
+        User receiver = this.userService.findUserById(transaction.receiverId());
+
+        userService.valideTransaction(sender, transaction.value());
+
+        if(this.authorizeTransaction(sender, transaction.value())){
+            
+        }
+    }
+
+    public boolean authorizeTransaction(User sender, BigDecimal value){
+       ResponseEntity<Map> authorizationResponse = restTemplate.getForEntity("https://run.mocky.io/v3/5794d450-d2e2-4412-8131-73d0293ac1cc", Map.class);
+
+       if(authorizationResponse.getStatusCode() == HttpStatus.OK){
+           String message = (String) authorizationResponse.getBody().get("message");
+           return "Autorizado".equalsIgnoreCase(message);
+       } else return false;
+    }
+}
