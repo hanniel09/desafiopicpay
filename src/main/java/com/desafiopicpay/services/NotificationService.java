51fbf13c29f9b3b4d1a2f7757b793694475fbca2
hanniel09 <hannielvieira1227@gmail.com>
2023-11-01 19:54:14 -0300

    feat: Update Notification service

--- src/main/java/com/desafiopicpay/services/NotificationService.java
+++ src/main/java/com/desafiopicpay/services/NotificationService.java
@@ -4,6 +4,7 @@ import com.desafiopicpay.domain.user.User;
 import com.desafiopicpay.dtos.NotificationDto;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -13,10 +14,15 @@ public class NotificationService {
     @Autowired
     private RestTemplate restTemplate;
 
-    public void sendNotification(User user, String message){
+    public void sendNotification(User user, String message) throws Exception {
         String email = user.getEmail();
         NotificationDto notificationRequest = new NotificationDto(email, message);
 
        ResponseEntity<String> notificationResponse = restTemplate.postForEntity("http://o4d9z.mocklab.io/notify", notificationRequest, String.class);
+
+       if(!(notificationResponse.getStatusCode() == HttpStatus.OK)){
+           System.out.println("Error to envie notification");
+           throw new Exception("Notification Service it's unavailable");
+       }
     }
 }
