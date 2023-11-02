a5d25857404dc277b4d7d3be8742c562b22673d4
hanniel09 <hannielvieira1227@gmail.com>
2023-11-01 23:15:20 -0300

    feat: Update users

--- src/main/java/com/desafiopicpay/services/UserService.java
+++ src/main/java/com/desafiopicpay/services/UserService.java
@@ -2,12 +2,15 @@ package com.desafiopicpay.services;
 
 import com.desafiopicpay.domain.user.User;
 import com.desafiopicpay.domain.user.UserType;
+import com.desafiopicpay.dtos.UserDto;
 import com.desafiopicpay.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class UserService {
@@ -29,8 +32,17 @@ public class UserService {
        return this.repository.findUserById(id).orElseThrow(() -> new Exception("User not found"));
     }
 
-    @Transactional
+    public User createUser(UserDto data){
+        User newUser = new User(data);
+        this.saveUser(newUser);
+        return newUser;
+    }
+
     public void saveUser(User user){
         this.repository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+      return this.repository.findAll();
     }
 }
