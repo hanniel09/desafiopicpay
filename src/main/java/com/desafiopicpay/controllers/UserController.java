bd9ef77328ed3683ed0cc7fb0bd238577cc7554c
hanniel09 <hannielvieira1227@gmail.com>
2023-11-01 23:15:37 -0300

    feat: create UserController

---
+++ src/main/java/com/desafiopicpay/controllers/UserController.java
@@ -0,0 +1,32 @@
+package com.desafiopicpay.controllers;
+
+import com.desafiopicpay.domain.user.User;
+import com.desafiopicpay.dtos.UserDto;
+import com.desafiopicpay.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController()
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody UserDto user){
+        User newUser = userService.createUser(user);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers(){
+        List<User> users = this.userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+}
