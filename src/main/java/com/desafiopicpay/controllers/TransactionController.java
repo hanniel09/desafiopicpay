74b28badc66f558c5fbc91ec160e8841793fec88
hanniel09 <hannielvieira1227@gmail.com>
2023-11-01 23:24:24 -0300

    feat: create TransactionController

---
+++ src/main/java/com/desafiopicpay/controllers/TransactionController.java
@@ -0,0 +1,10 @@
+package com.desafiopicpay.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("transactions")
+public class TransactionController {
+
+}
